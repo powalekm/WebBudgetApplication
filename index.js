3bcfa54da53ef4c9e13a98f2691db7dbaa32d74a
Michal Powalek <michal.powalek.programista@gmail.com>
2024-10-27 20:36:11 +0100

    PHP implementatation

--- index.js
+++ index.js
@@ -39,30 +39,3 @@ function buttonAnimation() {
         }
   }
 
-function displayModal(type) {
-    if (type == "signIn"){
-        if( !$("#pill-login").hasClass("active") ){
-            $("#pill-login").addClass("active");
-            $("#pill-register").removeClass("active");
-        }
-        if( !$("#pill-loginpanel").hasClass("active") && !$("#pill-loginpanel").hasClass("show") ){
-            $("#pill-registerpanel").removeClass("active");
-            $("#pill-registerpanel").removeClass("show");
-            $("#pill-loginpanel").addClass("active");
-            $("#pill-loginpanel").addClass("show");
-        }
-    } else if (type == "register"){
-        if( !$("#pill-register").hasClass("active") ){
-            $("#pill-register").addClass("active");
-            $("#pill-login").removeClass("active");
-        }
-        if( !$("#pill-registerpanel").hasClass("active") && !$("#pill-registerpanel").hasClass("show") ){
-            $("#pill-loginpanel").removeClass("active");
-            $("#pill-loginpanel").removeClass("show");
-            $("#pill-registerpanel").addClass("active");
-            $("#pill-registerpanel").addClass("show");
-        }
-    }else {
-        alert("Else bug");
-    }
-}
